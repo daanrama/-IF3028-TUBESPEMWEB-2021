923fb577c845c6e95c6c26a828979b003b0dabe3
Muhammad Ichsan Ramadhan <57926232+daanrama@users.noreply.github.com>
2020-12-19 22:16:25 +0700

    Upload models home

---
+++ application/models/Home_model.php
@@ -0,0 +1,29 @@
+<?php
+ 
+class Home_model extends CI_Model
+{
+	public function __construct()
+	{
+		$this->load->database();
+	}
+    public function Laporan()
+    {
+        return $this->db->get('laporan')->result_array();
+    }
+    public function cariJudul($special = false)
+	{
+		$judul = $this->input->get('judul', TRUE);
+		$data = $this->db->query("SELECT * from laporan where judul like '%$judul%' ");
+		if($special)
+		{
+			return $data;
+		}
+    return $data->result();
+	}
+
+	public function Tampilan($id)
+	{
+		return $this->db->get_where('laporan', array('id'=>$id))->row_array();
+	}
+}
+?>

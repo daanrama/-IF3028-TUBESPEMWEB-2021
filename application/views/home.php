694f4af1876f9cafeabb5f489d2c7ffa2949f240
Muhammad Ichsan Ramadhan <muhammad.118140126@student.itera.ac.id>
2020-12-19 12:35:08 +0700

    Upload default Codeigniter 3

---
+++ application/views/home.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LAPOR! - Layanan Aspirasi dan Pengaduan Online Rakyat</title>
+</head>
+<body>
+
+</body>
+</html>
